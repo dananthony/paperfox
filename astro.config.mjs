f51ad991e74a622f6252cb3ead0c2874cb38ae8a
Dan Anthony <dan@acolorbright.com>
2023-12-09 10:16:09 +0100

    chore: configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dananthony.github.io",
-  base: "/paperfox",
+  site: "https://www.paperfox.com.au",
 });
