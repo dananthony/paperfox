1727d8bf6b3b99809c1a52e13f60b69dbef5f69f
Dan Anthony <dan@acolorbright.com>
2023-12-10 13:30:32 +0100

    chore: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-recommended"],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+    },
+  ],
+};
